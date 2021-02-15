eecad6e6c953605fe07f72d27c3497b01753458c
lc5102 <49052946+1015355299@users.noreply.github.com>
2021-02-16 01:28:56 +0800

    push

--- index.js
+++ index.js
@@ -566,13 +566,13 @@ router.get('/props/:DeviceName', async (ctx, next) => {
 })
 
 // 获取设备单一属性的历史值
-router.get('/historyPropValue/:DeviceName', async (ctx, next) => {
-  // console.log(ctx.params,ctx.query);
+router.post('/historyPropValue', async (ctx, next) => {
+  const body = ctx.request.body
   try {
     var res = await getHistoryPropValue({
-      DeviceName: ctx.params.DeviceName,
-      FieldName: ctx.query.FieldName,
-      Limit: ctx.query.Limit * 1,
+      DeviceName: body.DeviceName,
+      FieldName: body.FieldName,
+      Limit: body.Limit * 1,
     })
   } catch (error) {}
   console.log(res)
